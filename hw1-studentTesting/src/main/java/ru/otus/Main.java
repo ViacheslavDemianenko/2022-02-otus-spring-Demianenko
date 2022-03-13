6a7656e8ea0cad933e48a97ca786a2eb9cf15d3f
Viacheslav Demianenko <urbanderwil@gmail.com>
2022-03-14 01:39:13 +0300

    HomeWork 1 (#1)
    * Первое домашнее задание без тестирования и толстого jar-ника
    
    * Поправил основной README
    
    * Поправил pom.xml

---
+++ hw1-studentTesting/src/main/java/ru/otus/Main.java
@@ -0,0 +1,17 @@
+package ru.otus;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.service.QuestionService;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "context.xml");
+
+        QuestionService questionService = context.getBean("questionServiceImpl", QuestionService.class);
+
+        questionService.printQuestions();
+
+        context.close();
+    }
+}
